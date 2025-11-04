feeaf4cadbfaf29484f83afe324b1f762614f6ae
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-04 14:03:56 +1100

    entry class

---
+++ Core/Entry.cs
@@ -0,0 +1,26 @@
+// entry
+
+namespace PWMan.Core;
+
+public class Entry : IdentifiableObject
+{
+    public Entry(string title, string username = "", string password = "", string notes = "", string url = "")
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Username = username;
+        Password = password;
+        Notes = notes;
+        Url = url;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public string Title { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Notes { get; set; }
+    public string Url { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
