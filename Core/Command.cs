8ea186dce91d9a1add3e5ef3d6dcc18c4928f0c6
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-05 00:41:16 +1100

    base help

--- Core/Command.cs
+++ Core/Command.cs
@@ -4,7 +4,15 @@ namespace PWMan.Core;
 
 public abstract class Command : IdentifiableObject
 {
-    public abstract bool Execute(string[] args);
+    public virtual string Execute(string[] args)
+    {
+        // Console.WriteLine ("args: " + string.Join(", ", args));
+        if (args.Length > 1 && (args[1] == "help" || args[1] == "h")) // "unlock help" (therefore check args[1])
+        {
+            return GetHelp();
+        }
+        return ""; // default implementation does nothing
+    }
 
     public virtual string GetHelp()
     {
