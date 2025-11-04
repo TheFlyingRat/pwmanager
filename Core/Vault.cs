b2e4e14a2be8cad639adbfaa2139637805f49233
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-05 00:40:57 +1100

    vault methods

--- Core/Vault.cs
+++ Core/Vault.cs
@@ -2,7 +2,6 @@
 // unlocked or locked right now as well as a decrypted-password cache
 // so we don't need to constantly decrypt if we're within the same
 // password manager session.
-using System.Text;
 using PWMan.Core.Encryption;
 using PWMan.Core.KeyDerivation;
 using PWMan.Data;
@@ -23,7 +22,7 @@ public class Vault
     {
         Encryption = new Caesar();
         KDF = new Argon2Derivation();
-        Repository = new JsonEntryRepository("vault.json", Encryption);
+        Repository = new JsonEntryRepository("vault.json", Encryption, "not used for caesar");
     }
 
     // accessor for our singleton instance
@@ -53,14 +52,23 @@ public class Vault
         {
             MasterKey = hash; // cache it on the vault instance
             IsLocked = false;
+
+            // load entries from repository into memory
+            _entries = Repository.GetAllEntries();
+            Console.WriteLine($"Loaded {_entries.Count} entries from repository.");
+            return true;
         }
 
-        return target == hash;
+        return false;
     }
 
     // locks vault and clears self cache to prevent previously decrypted passwords remaining in mem
     public void Lock()
     {
+        if (IsLocked)
+        {
+            throw new InvalidOperationException("Vault is already locked.");
+        }
         MasterKey = null;
         IsLocked = true;
 
@@ -78,6 +86,36 @@ public class Vault
 
         _entries.Add(entry);
         Repository.SaveEntry(entry);
+    }
+
+    public Entry GetEntry(Guid id)
+    {
+        if (IsLocked)
+        {
+            throw new InvalidOperationException("Vault is locked. Cannot get entry.");
+        }
+
+        return _entries.FirstOrDefault(e => e.Id == id) ?? throw new KeyNotFoundException($"Entry with ID '{id}' not found.");
+    }
+
+    public Entry SearchEntries(string title)
+    {
+        if (IsLocked)
+        {
+            throw new InvalidOperationException("Vault is locked. Cannot search entries.");
+        }
+
+        return _entries.FirstOrDefault(e => e.Title.Equals(title.ToLower())) ?? throw new KeyNotFoundException($"Entry with title '{title}' not found.");
+    }
+
+    public List<Entry> ListEntries()
+    {
+        if (IsLocked)
+        {
+            throw new InvalidOperationException("Vault is locked. Cannot get entries.");
+        }
+
+        return _entries;
     }
 
     public static bool Exists { get { return _instance != null; } }
