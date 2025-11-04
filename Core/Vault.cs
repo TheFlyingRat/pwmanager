fbdc6f1bd4f74bc3f186476c52dbcf41f0410ba3
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-04 15:41:27 +1100

    offload unlocking to a command

--- Core/Vault.cs
+++ Core/Vault.cs
@@ -12,18 +12,18 @@ namespace PWMan.Core;
 public class Vault
 {
     // singleton pattern, we only have one instance
-    private static Vault? _instance;
+    private static Vault? _instance = new Vault(); // eagerly create the instance
     private List<Entry> _entries = new List<Entry>();
-    private static IEntryRepository? _repo;
-    private static IEncryptionStrategy? _enc;
-    private static IKeyDerivation? _kdf; 
+    public static IEntryRepository? Repository { get; private set; }
+    public static IEncryptionStrategy? Encryption { get; private set; }
+    public static IKeyDerivation? KDF { get; private set; }
 
     // cant create a vault from outside of the actual class as constructor is private
     private Vault()
     {
-        _repo = new JsonEntryRepository("vault.json");
-        _enc = new Caesar();
-        _kdf = new Argon2Derivation();
+        Repository = new JsonEntryRepository("vault.json");
+        Encryption = new Caesar();
+        KDF = new Argon2Derivation();
     }
 
     // accessor for our singleton instance
@@ -40,38 +40,22 @@ public class Vault
         }
     }
     public bool IsLocked { get; private set; } = true; // default to locked state only allow unlocking by self masterkey
-    public byte[]? MasterKey { get; private set; } // cached masterkey when unlocked (kdfed)
+    public string? MasterKey { get; private set; } // cached masterkey when unlocked (kdfed)
 
 
-
-
-    // unlock vault with a given masterkey
-    public void Unlock(string masterKey)
+    // unlock vault with a given hash
+    public bool Unlock(string hash)
     {
- 
+        // TODO: determine password kdf so we know what derivation to use here target is stored in keyfile for now but will be in repo.
         string target = KeyFile.ReadFromFile("keyfile.txt");
-
-        byte[] salt = Convert.FromBase64String(target.Split('?')[1]);
-
-        byte[] key = _kdf.DeriveKey(masterKey ?? "", salt); // derive the key to cache
-
-        string hash = Convert.ToBase64String(key) + "?" + Convert.ToBase64String(salt);
-
-        Console.WriteLine("Full hash is: " + hash);
-
-
-        // TODO: determine password kdf so we know what derivation to use here
 
         if (target == hash) // replace with actual check
         {
-            MasterKey = key; // cache it on the vault instance
+            MasterKey = hash; // cache it on the vault instance
             IsLocked = false;
-            Console.WriteLine("Unlocked.");
         }
-        else
-        {
-            Console.WriteLine("Rejected.");
-        }
+
+        return target == hash;
     }
 
     // locks vault and clears self cache to prevent previously decrypted passwords remaining in mem
@@ -81,7 +65,20 @@ public class Vault
         IsLocked = true;
 
         // clear entries from memory as well 
-        _repo.SaveAllEntries(_entries); // save before clearing
+        Repository.SaveAllEntries(_entries); // save before clearing
         _entries.Clear();
     }
+
+    public void AddEntry(Entry entry)
+    {
+        if (IsLocked)
+        {
+            throw new InvalidOperationException("Vault is locked. Cannot add entry.");
+        }
+
+        _entries.Add(entry);
+        Repository.SaveEntry(entry);
+    }
+
+    public static bool Exists { get { return _instance != null; } }
 }
