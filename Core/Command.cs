05f0929530392de95f5337c9a5af05e374f42bc1
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-04 14:04:41 +1100

    command base class

---
+++ Core/Command.cs
@@ -0,0 +1,15 @@
+// command abstract base class
+
+namespace PWMan.Core;
+
+public abstract class Command : IdentifiableObject
+{
+    public abstract void Execute(string[] args);
+
+    public virtual string GetHelp()
+    {
+        return "No help available for this command.";
+    }
+
+    public abstract string Name { get; protected set; }
+}
