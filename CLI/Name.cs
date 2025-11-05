93a9c1e32118a1798253fb3ce746eca6f3d60adb
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-05 16:51:53 +1100

    offload into static data classes

---
+++ CLI/Name.cs
@@ -0,0 +1,11 @@
+namespace PWMan.CLI
+{
+    public static class Name
+    {
+        public const string StylisedAppName = "PWMan";
+        public const string Author = "Joey Manani";
+        public const string Copyright = "© 2025 Joey Manani. All rights reserved.";
+        public const string CLIName = "pwman";
+        public const string Description = "PWManager - todo";
+    }
+}
---
+++ CLI/Version.cs
@@ -0,0 +1,7 @@
+namespace PWMan.CLI
+{
+    public static class Version
+    {
+        public const string AppVersion = "0.1.0";
+    }
+}
