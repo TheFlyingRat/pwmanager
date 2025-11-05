f4bd2c58480c5f4707823c8b2c1ceb10669e3f57
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-05 16:57:03 +1100

    use REPL

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PWMan.CLI;
+using PWMan.Commands;
+using PWMan.Core;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<Command> commands = new List<Command>
+        {
+            new UnlockVaultCommand(),
+            new LockVaultCommand(),
+            new AddEntryCommand(),
+            new ListEntriesCommand()
+        };
+
+        REPL repl = new REPL(commands);
+        repl.Start();
+    }
+}
