52705b31039e6cff25a2287d47b6feab8a51bd57
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 12:31:38 +1100

    update validation types

--- CLI/GetDefaultValidate.cs
+++ CLI/GetDefaultValidate.cs
@@ -16,14 +16,24 @@ public static class GetDefaultValidate
         return string.IsNullOrWhiteSpace(input) ? null : Convert.ToInt32(input);
     }
 
-    public static string ValidateQuery(string query, string[] acceptedValues, string defaultValue)
+    public static string ValidateString(string query, string[] acceptedValues, string defaultValue)
     {
         Console.WriteLine(query);
         string input = Console.ReadLine();
         return String(acceptedValues, input, defaultValue);
     }
 
-    public static string String(string[] acceptedValues, string inputValue, string defaultValue)
+    public static int ValidateInt(string query, int min, int max, int defaultValue)
+    {
+        Console.WriteLine(query);
+        string input = Console.ReadLine();
+        int value;
+        try { value = Convert.ToInt32(input); } catch { value = defaultValue; } // convert to string (unsuccessful use default)
+        if (min > value || value < max) { value = defaultValue; } // was given int? okay range check
+        return value;
+    }
+
+    private static string String(string[] acceptedValues, string inputValue, string defaultValue)
     {
         if (!acceptedValues.Contains(inputValue.ToLower()))
         {
