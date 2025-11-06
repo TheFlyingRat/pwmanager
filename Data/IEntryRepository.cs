6f35a114fb3839265229986592e502915def82db
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 15:24:45 +1100

    no longer handled by repo, rather the vault itself

--- Data/IEntryRepository.cs
+++ Data/IEntryRepository.cs
@@ -1,17 +1,10 @@
-// interface because during development we have an in-memory repository and during production
-// we will be using the sqlite one - so we need the repositories to implement this interface
-
 using PWMan.Core;
 
 namespace PWMan.Data;
 
 public interface IEntryRepository
 {
-    void SaveEntry(Entry entry);
     Entry? GetEntry(Guid id);
-    List<Entry> GetAllEntries();
-    void DeleteEntry(Guid id);
-    void SaveAllEntries(List<Entry> entries);
     void Create();
     void LoadVault();
     void SaveVault();
