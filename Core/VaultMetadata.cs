1e4da8d17df28d99c665526b67b20572486d27a7
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 12:34:12 +1100

    vault metadata for enveloping

---
+++ Core/VaultMetadata.cs
@@ -0,0 +1,14 @@
+namespace PWMan.Core;
+
+public class VaultMetadata
+{
+    public string EncryptionMethod { get; set; } = "aes";
+    public string DerivationMethod { get; set; } = "argon2";
+    public string RepositoryType { get; set; } = "json";
+    public string SaveFile { get; set; } = "vault.json";
+    public int SaltSize { get; set; } = 16;
+    public int KeySize { get; set; } = 32;
+    public int Iterations { get; set; } = 100000;
+    public byte[]? Salt { get; set; }
+    public string? Key { get; set; }
+}
