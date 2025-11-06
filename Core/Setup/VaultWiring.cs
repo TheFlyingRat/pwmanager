c35e83cfc5c5e8f8b6567f92505fd6f69395732c
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 12:56:31 +1100

    factory patterns

---
+++ Core/Setup/VaultWiring.cs
@@ -0,0 +1,70 @@
+using System.Security.Cryptography;
+using PWMan.Core.Encryption;
+using PWMan.Core.KeyDerivation;
+using PWMan.Data;
+
+namespace PWMan.Core.Setup
+{
+    public static class VaultWiring
+    {
+
+        // helper method to determine encryption type to use based on options
+        public static IEncryptionStrategy BuildEncryption(NewVaultOptions options)
+        {
+            switch (options.Encryption.ToLower())
+            {
+                case "caesar":
+                    return new Caesar();
+                case "aes":
+                default:
+                    return new AES(saltSize: options.SaltSize);
+            }
+        }
+
+        // helper to determine kdf based on options
+        public static IKeyDerivation BuildKdf(NewVaultOptions options)
+        {
+            switch (options.Kdf.ToLower())
+            {
+                case "argon2":
+                    return new Argon2Derivation(iterations: options.Iterations, keySize: options.KeySize);
+                case "pbkdf2":
+                default:
+                    return new Pbkdf2Derivation(iterations: options.Iterations, keySize: options.KeySize);
+            }
+        }
+
+        // helper to determine repo type based on options
+        public static IEntryRepository BuildRepository(NewVaultOptions options, IEncryptionStrategy enc)
+        {
+            switch (options.SaveType.ToLower())
+            {
+                case "json":
+                default:
+                    return new JsonEntryRepository(options.SaveFile, enc);
+            }
+        }
+
+        // helper to save the temporary options data to the metadata of the vault
+        public static void ApplyMetadata(VaultMetadata meta, NewVaultOptions options)
+        {
+            meta.EncryptionMethod = options.Encryption;
+            meta.DerivationMethod = options.Kdf;
+            meta.Iterations = options.Iterations;
+            meta.KeySize = options.KeySize;
+            meta.SaltSize = options.SaltSize;
+            meta.RepositoryType = options.SaveType;
+            meta.SaveFile = options.SaveFile;
+            meta.Salt = RandomNumberGenerator.GetBytes(options.SaltSize);
+        }
+
+        // helper to generate the vault (not instantiated until Vault.Instance is referenced because singleton)
+        public static void WireVault(IEncryptionStrategy enc, IKeyDerivation kdf, IEntryRepository repo, string password)
+        {
+            Vault.Encryption = enc;
+            Vault.KDF = kdf;
+            Vault.Repository = repo;
+            Vault.RuntimePassword = password;
+        }
+    }
+}
