39e4ee621cced63b03de87dd8471f4d900bfcfc5
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 23:24:43 +1100

    validation helpers

--- CLI/GetDefaultValidate.cs
+++ CLI/GetDefaultValidate.cs
@@ -1,3 +1,5 @@
+using PWMan.Core;
+
 namespace PWMan.CLI;
 
 public static class GetDefaultValidate
@@ -5,7 +7,7 @@ public static class GetDefaultValidate
     public static string GetString(string query, string defaultValue)
     {
         Console.Write(query);
-        string input = Console.ReadLine();
+        string input = Console.ReadLine().Trim();
         return string.IsNullOrWhiteSpace(input) ? defaultValue : input;
     }
 
@@ -15,11 +17,81 @@ public static class GetDefaultValidate
         while (input == null)
         {
             Console.Write(query);
-            string value = Console.ReadLine();
+            string value = Console.ReadLine().Trim();
             input = string.IsNullOrWhiteSpace(value) ? null : value;
         }
         return input;
     }
+
+    public static EncryptionType ValidateEncryption(string prompt, EncryptionType defaultValue)
+    {
+        // get all names within the enum
+        string[] options = Enum.GetNames<EncryptionType>();
+
+        // prompt
+        Console.Write($"{prompt} ({string.Join(", ", options)}) [{defaultValue}]: ");
+        string input = Console.ReadLine().Trim();
+
+        if (string.IsNullOrWhiteSpace(input)) { return defaultValue; }
+
+        // try to convert the input string into an EncryptionType (case-insensitive)
+        try
+        {
+            return Enum.Parse<EncryptionType>(input, ignoreCase: true);
+        }
+        catch
+        {
+            Console.WriteLine($"Invalid choice. Using default: {defaultValue}");
+            return defaultValue;
+        }
+    }
+
+    public static DerivationType ValidateDerivation(string prompt, DerivationType defaultValue)
+    {
+        // get all names within the enum
+        string[] options = Enum.GetNames<DerivationType>();
+
+        // prompt
+        Console.Write($"{prompt} ({string.Join(", ", options)}) [{defaultValue}]: ");
+        string input = Console.ReadLine().Trim();
+
+        if (string.IsNullOrWhiteSpace(input)) { return defaultValue; }
+
+        // try to convert the input string into an DerivationType (case-insensitive)
+        try
+        {
+            return Enum.Parse<DerivationType>(input, ignoreCase: true);
+        }
+        catch
+        {
+            Console.WriteLine($"Invalid choice. Using default: {defaultValue}");
+            return defaultValue;
+        }
+    }
+    
+    public static RepositoryType ValidateRepositoryType(string prompt, RepositoryType defaultValue)
+    {
+        // get all names within the enum
+        string[] options = Enum.GetNames<RepositoryType>();
+
+        // prompt
+        Console.Write($"{prompt} ({string.Join(", ", options)}) [{defaultValue}]: ");
+        string input = Console.ReadLine().Trim();
+
+        if (string.IsNullOrWhiteSpace(input)) { return defaultValue; }
+        
+        // try to convert the input string into an RepositoryType (case-insensitive)
+        try
+        {
+            return Enum.Parse<RepositoryType>(input, ignoreCase: true);
+        }
+        catch
+        {
+            Console.WriteLine($"Invalid choice. Using default: {defaultValue}");
+            return defaultValue;
+        }
+    }
+
 
     public static int? GetInt(string query)
     {
@@ -31,17 +103,17 @@ public static class GetDefaultValidate
     public static string ValidateString(string query, string[] acceptedValues, string defaultValue)
     {
         Console.Write(query);
-        string input = Console.ReadLine();
+        string input = Console.ReadLine().Trim();
         return String(acceptedValues, input, defaultValue);
     }
 
     public static int ValidateInt(string query, int min, int max, int defaultValue)
     {
         Console.Write(query);
-        string input = Console.ReadLine();
+        string input = Console.ReadLine().Trim();
         int value;
-        try { value = Convert.ToInt32(input); } catch { value = defaultValue; } // convert to string (unsuccessful use default)
-        if (value < min || value > max) { value = defaultValue; } // was given int? okay range check
+        try { value = Convert.ToInt32(input); } catch { Console.WriteLine("Invalid choice. Using default: " + defaultValue); value = defaultValue; } // convert to string (unsuccessful use default)
+        if (value < min || value > max) { Console.WriteLine("Invalid choice. Using default: " + defaultValue); value = defaultValue; } // was given int? okay range check
         return value;
     }
 
