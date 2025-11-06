30834af9481280adba6a12687fc3842109f51dd2
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 12:53:10 +1100

    GetStringRequired helper

--- CLI/GetDefaultValidate.cs
+++ CLI/GetDefaultValidate.cs
@@ -2,30 +2,42 @@ namespace PWMan.CLI;
 
 public static class GetDefaultValidate
 {
-    public static string? GetString(string query)
+    public static string GetString(string query, string defaultValue)
     {
-        Console.WriteLine(query);
+        Console.Write(query);
         string input = Console.ReadLine();
-        return string.IsNullOrWhiteSpace(input) ? null : input;
+        return string.IsNullOrWhiteSpace(input) ? defaultValue : input;
+    }
+
+    public static string GetStringRequired(string query)
+    {
+        string? input = null;
+        while (input == null)
+        {
+            Console.Write(query);
+            string value = Console.ReadLine();
+            input = string.IsNullOrWhiteSpace(value) ? null : value;
+        }
+        return input;
     }
 
     public static int? GetInt(string query)
     {
-        Console.WriteLine(query);
+        Console.Write(query);
         string input = Console.ReadLine();
         return string.IsNullOrWhiteSpace(input) ? null : Convert.ToInt32(input);
     }
 
     public static string ValidateString(string query, string[] acceptedValues, string defaultValue)
     {
-        Console.WriteLine(query);
+        Console.Write(query);
         string input = Console.ReadLine();
         return String(acceptedValues, input, defaultValue);
     }
 
     public static int ValidateInt(string query, int min, int max, int defaultValue)
     {
-        Console.WriteLine(query);
+        Console.Write(query);
         string input = Console.ReadLine();
         int value;
         try { value = Convert.ToInt32(input); } catch { value = defaultValue; } // convert to string (unsuccessful use default)
