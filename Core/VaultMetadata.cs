9fdc07db3f638b009afe7289288e13c49d395b4f
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 17:33:36 +1100

    remove default values since they caused issues

--- Core/VaultMetadata.cs
+++ Core/VaultMetadata.cs
@@ -2,11 +2,11 @@ namespace PWMan.Core;
 
 public class VaultMetadata
 {
-    public string EncryptionMethod { get; set; } = "aes";
-    public string DerivationMethod { get; set; } = "argon2";
-    public string RepositoryType { get; set; } = "json";
-    public string SaveFile { get; set; } = "vault.json";
-    public int SaltSize { get; set; } = 16;
-    public int KeySize { get; set; } = 32;
-    public int Iterations { get; set; } = 100000;
+    public string? EncryptionMethod { get; set; }
+    public string? DerivationMethod { get; set; }
+    public string? RepositoryType { get; set; }
+    public string? SaveFile { get; set; }
+    public int SaltSize { get; set; }
+    public int KeySize { get; set; }
+    public int Iterations { get; set; }
 }
