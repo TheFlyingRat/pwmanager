669f1e4114410b9c4753b50836254f7f129de5c1
TheFlyingRat <contact@mail.theflyingrat.com>
2025-11-06 15:24:19 +1100

    utilise salt class and refinement

--- Core/Encryption/AES.cs
+++ Core/Encryption/AES.cs
@@ -1,4 +1,3 @@
-using PWMan.Core.KeyDerivation;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -6,88 +5,89 @@ namespace PWMan.Core.Encryption
 {
     public class AES : IEncryptionStrategy
     {
-        // aes-gcm standards
-        private int SaltSize { get; set; }
-        private int NonceSize { get; set; }
-        private int TagSize { get; set; }
-        private readonly Pbkdf2Derivation KDF;
+        // aes-gcm defaults
+        private int SaltSize { get; }
+        private int NonceSize { get; }
+        private int TagSize { get; }
 
-        public AES(int saltSize = 16, int nonceSize = 12, int tagSize = 16, int KDFIterations = 100_000, int KDFKeySize = 32)
+        public AES(int saltSize = 16, int nonceSize = 12, int tagSize = 16)
         {
             SaltSize = saltSize;
             NonceSize = nonceSize;
             TagSize = tagSize;
-            KDF = new Pbkdf2Derivation(KDFIterations, KDFKeySize);
         }
-        
 
         public string Encrypt(string plaintext, string password)
         {
-            // 1. derive key from password + random salt
-            byte[] salt = new byte[SaltSize];
-            RandomNumberGenerator.Fill(salt);
-            byte[] key = KDF.DeriveKey(password, salt);
+            // 1) fresh salt per encryption
+            var salt = Salt.GenerateSalt(SaltSize);
 
-            // 2. make random nonce (randomness)
-            byte[] nonce = new byte[NonceSize];
-            RandomNumberGenerator.Fill(nonce);
+            // 2) derive key from password + salt
+            var key = Vault.Instance.KDF.DeriveKey(password, salt);
 
-            // 3. encrypt
-            byte[] plainBytes = Encoding.UTF8.GetBytes(plaintext); // convert data to bytes
-            byte[] cipherBytes = new byte[plainBytes.Length];
-            byte[] tag = new byte[TagSize];
-            var aes = new AesGcm(key, TagSize);
-            aes.Encrypt(nonce, plainBytes, cipherBytes, tag); // encrypt the plainBytes into cipherBytes with tag for integrity, nonce for randomness
+            // 3) fresh nonce per encryption
+            var nonce = Salt.GenerateSalt(NonceSize);
 
+            // 4) start encoding
+            var pt = Encoding.UTF8.GetBytes(plaintext);
+            var ct = new byte[pt.Length];
+            var tag = new byte[TagSize];
 
-            // 4. package to a base64 string: [salt | nonce | tag | ciphertext]
+            var aes = new AesGcm(key, TagSize); // utilise derived key and tagsize for integrity
 
-            // ============== offset the blocks correctly into the byte array (AI helped with this idea)
-            byte[] output = new byte[SaltSize + NonceSize + TagSize + cipherBytes.Length];
-            Buffer.BlockCopy(salt,        0, output, 0,                              SaltSize);
-            Buffer.BlockCopy(nonce,       0, output, SaltSize,                       NonceSize);
-            Buffer.BlockCopy(tag,         0, output, SaltSize + NonceSize,           TagSize);
-            Buffer.BlockCopy(cipherBytes, 0, output, SaltSize + NonceSize + TagSize, cipherBytes.Length);
+            aes.Encrypt(nonce, pt, ct, tag); // encrypt
 
-            // salt start offset 0, into the output, 0 bytes from the start of the destination array, for the length of SaltSize
+            // 5) pack [salt|nonce|tag|ciphertext] to a base64 string
+            var output = new byte[SaltSize + NonceSize + TagSize + ct.Length];
+            Buffer.BlockCopy(salt,  0, output, 0,                         SaltSize);
+            Buffer.BlockCopy(nonce, 0, output, SaltSize,                  NonceSize);
+            Buffer.BlockCopy(tag,   0, output, SaltSize + NonceSize,      TagSize);
+            Buffer.BlockCopy(ct,    0, output, SaltSize + NonceSize + TagSize, ct.Length);
 
             return Convert.ToBase64String(output);
-
-            // ==============
         }
 
         public string Decrypt(string ciphertext, string password)
         {
+            var input = Convert.FromBase64String(ciphertext);
 
-            // 1. unpack base64: [salt | nonce | tag | ciphertext] (assumes the encryption used same size for decryption - shouldn't change.)
-            // TODO: config file!!
-            byte[] input = Convert.FromBase64String(ciphertext);
-            byte[] salt  = new byte[SaltSize];
-            byte[] nonce = new byte[NonceSize];
-            byte[] tag = new byte[TagSize];
+            // sanity check first (there needs to be some data beyond the end of the byte lengths)
+            if (input.Length < SaltSize + NonceSize + TagSize) {
+                throw new CryptographicException("Ciphertext too short.");
+            };
 
-            // unpack from input to respective byte arrays from the offset as the second param
-            // TODO: something to talk about in presentation 
-            Buffer.BlockCopy(input, 0,                         salt,  0, SaltSize);
+            // 1) unpack [salt|nonce|tag|ciphertext]
+            var salt  = new byte[SaltSize];
+            var nonce = new byte[NonceSize];
+            var tag   = new byte[TagSize];
+
+            // derive from the byte array
+            Buffer.BlockCopy(input, 0,                         salt,  0, SaltSize); 
             Buffer.BlockCopy(input, SaltSize,                  nonce, 0, NonceSize);
             Buffer.BlockCopy(input, SaltSize + NonceSize,      tag,   0, TagSize);
 
-            // calculate message length
-            int cipherLen = input.Length - (SaltSize + NonceSize + TagSize);
-            byte[] cipherBytes = new byte[cipherLen];
+            // ciphertext length is the total length of the incoming encrypted byte length subtracting the size of salt, nonce and tag 
+            var ctLen = input.Length - (SaltSize + NonceSize + TagSize);
+            var ct = new byte[ctLen];
+            Buffer.BlockCopy(input,     SaltSize + NonceSize + TagSize,     ct, 0, ctLen); // extract the actual encrypted data
 
-            // copy input starting from the cipher offset to cipherBytes length into cypherBytes array
-            Buffer.BlockCopy(input,   SaltSize + NonceSize + TagSize,   cipherBytes, 0, cipherLen);
+            // 2) re-derive key
+            var key = Vault.Instance.KDF.DeriveKey(password, salt);
 
-            // 2. re-derive key for decryption
-            byte[] key = KDF.DeriveKey(password, salt);
+            // 3) decrypt
+            var pt = new byte[ct.Length]; // plaintext same size as ciphertext
+            try
+            {
+                var aes = new AesGcm(key, TagSize);
 
-            // 3. try decrypt
-            byte[] plainBytes = new byte[cipherBytes.Length]; // decrypted data will be same length as cipher
-            var aes = new AesGcm(key, TagSize);
-            aes.Decrypt(nonce, cipherBytes, tag, plainBytes);
-
-            return Encoding.UTF8.GetString(plainBytes);
+                aes.Decrypt(nonce, ct, tag, pt); // try decrypt
+            }
+            catch (CryptographicException)
+            {
+                // wrong password
+                throw new UnauthorizedAccessException("Decryption failed.");
+            }
+            return Encoding.UTF8.GetString(pt); // decrypted data
         }
     }
 }
